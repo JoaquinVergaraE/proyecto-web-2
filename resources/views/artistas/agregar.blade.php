75301e561a98de3d8bb3e2d3b6297f4a40cc0127
ian-usm <ian.dodman@usm.cl>
2023-07-01 01:48:38 -0400

    subir foto, gestionar y index de artista arreglados

--- resources/views/artistas/agregar.blade.php
+++ resources/views/artistas/agregar.blade.php
@@ -1,14 +1,24 @@
-<form action="{{ route('artistas.guardarImagen') }}" method="POST" enctype="multipart/form-data">
-    @csrf
-    <div>
-        <label for="titulo">Titulo:</label>
-        <input type="text" name="titulo" id="titulo" required>
-    </div>
-    <div>
-        <label for="archivo">Seleccionar archivo:</label>
-        <input type="file" name="imagenn" id="imagenn" required>
-    </div>
-    <div>
-        <button type="submit">Guardar imagen</button>
-    </div>
-</form>
+@extends('templates.master')
+
+@section('contenido-principal')
+<div class="bg-primary p-4 rounded-4 text-info ">
+    <form action="{{ route('artistas.guardarImagen') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        <div class="input-group mt-2 d-flex container">
+            <div class="input-group-text bg-info">
+                <span class="material-icons md-48">&#xe161;</span>
+            </div>
+            <input class="form-control" type="text" name="titulo" id="titulo" placeholder="Título">
+        </div>
+        <div class=" flex container mt-2">
+            <input class="form-control form-control-sm bg-secondary" id="imagenn" required name="imagenn" type="file">
+        </div>
+        <div class="d-flex mt-3">
+            <button type="submit" class="btn btn-info fw-semibold justify-content-center w-100">Guardar imagen</button>
+        </div>
+    </form>
+</div>
+
+
+@endsection
+
--- resources/views/templates/example.blade.php
+++ resources/views/templates/example.blade.php
@@ -2,15 +2,21 @@
 
 @section('contenido-principal')
 <div class="bg-primary p-4 rounded-4 text-info ">
-    <div class="d-flex justify-content-center md-48 ">
-        <span class="material-icons" >&#xe3ae;</span>
-        
-    </div>
-    <div class="text-center fs-1 fw-semibold">
-        Opciones
-    </div>
-    <div class="list-group">
-        <a href="{{route('artistas.gestionar')}}" class="list-group-item">Gestionar fotos</a>
-        <a href="#" class="list-group-item">Ver fotos baneadas</a>
-    </div>
+    <form action="{{ route('artistas.guardarImagen') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        <div>
+            <label for="titulo">Titulo:</label>
+            <input type="text" name="titulo" id="titulo" required>
+        </div>
+        <div>
+            <label for="archivo">Seleccionar archivo:</label>
+            <input type="file" name="imagenn" id="imagenn" required>
+        </div>
+        <div>
+            <button type="submit">Guardar imagen</button>
+        </div>
+    </form>
+</div>
+
+
 @endsection
